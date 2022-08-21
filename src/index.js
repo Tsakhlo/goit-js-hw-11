5d69649ecfa9a49890215d4ceb9863ef65152cc7
Oleksii <97940139+Tsakhlo@users.noreply.github.com>
2022-08-21 22:49:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import Notiflix from "notiflix";
 import SimpleLightbox from "simplelightbox";
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+  
+  let getGallery;
+  let page = 1;
 
